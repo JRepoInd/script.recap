3d868cac1057f086a2efbe369a174218360fbe58
rhudson <rhudson@rhudson-w530.emea.tibco.com>
2019-04-24 09:25:55 +0100

    Update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+__NOTICE__
+
+__This addon is no longer maintained by me, if you are interested in taking it over then please contact me via the Issues section. (You are welcome to submit it to the official Kodi Repo - just make sure you remove my name from it before you do it)__
+
+
 ![Recap](icon.png)
 
 Supported by:
@@ -13,15 +18,3 @@ Please give it a go, and feel free to post any suggestions/issues. It can be run
 For more information please see the wiki:
 
 [Add-on:Recap](https://github.com/robwebset/script.recap/wiki)
-
-You can install this addon from: [Repository:URepo](http://www.urepo.org/)
-
-For latest developer version: [Repository:robwebset](https://github.com/robwebset/repository.robwebset/blob/master/repos/repository.robwebset/repository.robwebset-1.0.0.zip) ([wiki](https://github.com/robwebset/repository.robwebset/wiki))
-
-To ask questions or comment about Recap, please use the forum at [URepo.org](http://www.urepo.org/forum/viewtopic.php?t=1761)
-
-__Donations__
-
-If you have found Recap useful and would like to help support future development and bug fixes, then please feel free to make a small donation.
-
-[![Donation](https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2NRERY5WWNFVL)
--- resources/lib/core.py
+++ resources/lib/core.py
@@ -92,47 +92,26 @@ def runRecap(previous=False):
 
     showName = None
 
-    json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.robwebset", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
-    json_response = simplejson.loads(json_query)
+    # First check to see if we have a TV Show already selected
+    if xbmc.getInfoLabel("ListItem.dbtype") in ['tvshow', 'season', 'episode']:
+        showName = xbmc.getInfoLabel("ListItem.TVShowTitle")
 
-    displayNotice = True
-    if ("result" in json_response) and ('addon' in json_response['result']):
-        addonItem = json_response['result']['addon']
-        if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
-            displayNotice = False
+    autoDetectedShowName = False
+    # If there is no video name available prompt for it
+    if showName in [None, ""]:
+        log("Recap: No TV Show detected, prompting user")
 
-    if displayNotice:
-        json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.urepo", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
-        json_response = simplejson.loads(json_query)
+        # Prompt the user for the new name
+        keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(32014), False)
+        keyboard.doModal()
 
-        if ("result" in json_response) and ('addon' in json_response['result']):
-            addonItem = json_response['result']['addon']
-            if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.urepo'):
-                displayNotice = False
-
-    if displayNotice:
-        xbmc.executebuiltin('Notification("robwebset or URepo Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
+        if keyboard.isConfirmed():
+            try:
+                showName = keyboard.getText().decode("utf-8")
+            except:
+                showName = keyboard.getText()
     else:
-        # First check to see if we have a TV Show already selected
-        if xbmc.getInfoLabel("ListItem.dbtype") in ['tvshow', 'season', 'episode']:
-            showName = xbmc.getInfoLabel("ListItem.TVShowTitle")
-
-        autoDetectedShowName = False
-        # If there is no video name available prompt for it
-        if showName in [None, ""]:
-            log("Recap: No TV Show detected, prompting user")
-
-            # Prompt the user for the new name
-            keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(32014), False)
-            keyboard.doModal()
-
-            if keyboard.isConfirmed():
-                try:
-                    showName = keyboard.getText().decode("utf-8")
-                except:
-                    showName = keyboard.getText()
-        else:
-            autoDetectedShowName = True
+        autoDetectedShowName = True
 
     recap = Recap()
     selectedItem = None
